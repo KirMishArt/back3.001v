f47d6402e4f1ae76982257d9b6f5cc1cf1fe1403
artem-evko <evko_artem@mail.ru>
2024-03-11 21:41:48 +0300

    qwerty

--- src/main/java/back/third/servlets/AddServlet.java
+++ src/main/java/back/third/servlets/AddServlet.java
@@ -11,20 +11,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @WebServlet("/add")
 public class AddServlet extends HttpServlet {
+
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
-        Person person;
-        request.setCharacterEncoding("UTF-16");
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
-        String second_name = request.getParameter("second_name");
-        String tel = request.getParameter("tel");
-        String email = request.getParameter("email");
-        String birth = request.getParameter("birth");
-        String gender = request.getParameter("gender");
-        String fovolang = request.getParameter("fovolang");
-        String bio = request.getParameter("bio");
-        person=new Person(name,surname,second_name,tel,email,birth,gender,fovolang,bio);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name= req.getParameter("name");
+        String surname= req.getParameter("surname");
+        String second_name= req.getParameter("second_name");
+        String tel= req.getParameter("tel");
+        String email= req.getParameter("email");
+        String birth= req.getParameter("birth");
+        String gender= req.getParameter("gender");
+        String fovolang= req.getParameter("fovolang");
+        String bio= req.getParameter("bio");
+        Person person=new Person(name,surname,second_name,tel,email,birth,gender,fovolang,bio);
         PersonDAO personDAO=new PersonDAO();
         personDAO.addPerson(person);
         resp.sendRedirect("/back3_war/persons");
